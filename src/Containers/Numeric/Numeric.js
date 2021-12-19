2f8fd9a115c7e48296f7f31de86c21dece9aa750
adilkhot81 <36324059+adilkhot81@users.noreply.github.com>
2021-12-19 23:15:32 +0530

    Aligned the Numeric buttons horizontally, few css addition to numeric component

--- src/Containers/Numeric/Numeric.js
+++ src/Containers/Numeric/Numeric.js
@@ -1,16 +1,17 @@
 import React,{useState} from 'react';
-import Aux from '../../HOC/Auxx/Auxx'
+//import Aux from '../../HOC/Auxx/Auxx'
 import Button from '../../Components/Button/Button';
+import classes from './numeric.module.css'
 
 const Numeric =()=> {
   const [count, setCount]=useState(0);
   
     return(
-      <Aux>
+      <div className={classes.numeric}>
         <Button label="(-)" click={()=>setCount(count - 1)}/>
         <div>{count}</div>
         <Button label="(+)" click={()=>setCount(count + 1)}/>
-      </Aux>
+      </div>
     );
   
 }
